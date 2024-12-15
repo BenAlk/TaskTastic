5dcb6b007af8eb2b06bafbf7f508c761c5047aaf
BenAlk <benalkureishi@hotmail.co.uk>
2024-12-15 22:31:31 +0000

    s

--- client/src/components/layout/Layout.jsx
+++ client/src/components/layout/Layout.jsx
@@ -1,6 +1,6 @@
 import { Outlet } from 'react-router-dom';
 import Header from './Header/Header';
-import Navbar from './Navbar/Navbar';
+import Navbar from './NavBar/NavBar';
 import styles from "./Layout.module.css"
 
 const Layout = () => {

687301a14e938d98d671d90b28eef7e80c4f17ec
Ben Alkureishi <benalkureishi@hotmail.co.uk>
2025-01-20 22:56:26 +0000

    updated CORS

--- server/src/index.js
+++ server/src/index.js
@@ -2,7 +2,6 @@ import express from 'express'
 import cors from 'cors'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
-
 import { authRouter } from './routes/auth.js'
 import { userRouter } from './routes/users.js'
 import { projectRouter } from './routes/projects.js'
@@ -10,12 +9,17 @@ import { taskRouter } from './routes/tasks.js'
 import { messageRouter } from './routes/messages.js'
 
 dotenv.config({ path: '.env.local' })
-
 const MONGO_DB_PASSWORD = process.env.MONGO_DB_PASSWORD
-const app = express()
 
+const app = express()
 app.use(express.json())
-app.use(cors())
+
+app.use(cors({
+    origin: ['https://tasktastic-testing.netlify.app', 'http://localhost:5173'],
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}))
 
 app.use("/auth", authRouter)
 app.use("/users", userRouter)
